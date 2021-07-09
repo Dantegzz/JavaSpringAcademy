e380aa236df24c02b800556c5569917495523ee2
Dante Gonzalez <dantegzz97@gmail.com>
2021-07-09 17:18:19 -0500

    last update to project files

--- dante-market/src/main/java/com/dante/market/DanteMarketApplication.java
+++ dante-market/src/main/java/com/dante/market/DanteMarketApplication.java
@@ -1,4 +1,4 @@
-package com.Dann97.market;
+package com.dante.market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- dante-market/src/test/java/com/dante/market/DanteMarketApplicationTests.java
+++ dante-market/src/test/java/com/dante/market/DanteMarketApplicationTests.java
@@ -1,4 +1,4 @@
-package com.Dann97.market;
+package com.dante.market;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
